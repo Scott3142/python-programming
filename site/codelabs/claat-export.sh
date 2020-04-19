3cdc15d549ab3b0fd298d57edaf3d5c61c99aab4
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-19 15:28:54 +0100

    Fixes broken url for pressing Done at end of codelab

--- site/codelabs/claat-export.sh
+++ site/codelabs/claat-export.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-~/go/bin/claat export *.md;
-~/go/bin/claat serve
+#This file 'compiles' and exports every codelab in this directory
+#We have to manually change the place the codelabs look for their formatting to our custom directory
+#This fixes the path executed when user presses 'Done' at the end of the codelab.
+claat export *.md
+find . -type f -name 'index.html' -exec sed -i 's/https:\/\/storage\.googleapis\.com\/codelab-elements\/codelab-elements\.js/\.\.\/\.\.\/elements\/codelab-elements\/codelab-elements\.js/g' {} \;
 
-
+claat serve
